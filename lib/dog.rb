eeadbb608da6c104027cef1a100fb3219d2a0a79
ogola5 <ogolaevance5@gmail.com>
2022-10-21 06:57:20 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    # setter method
+    def name=(value)
+        # assign the @name instance variable a value of whatever is passed as an argument
+        @name = value
+    end
 
+    # getter method
+    def name
+        # access the data from the @name instance variable and return it
+        @name
+    end
 end
+
+odie = Dog.new
+odie.name = "Odie"
+
+# call the Dog#name method
+puts odie.name
